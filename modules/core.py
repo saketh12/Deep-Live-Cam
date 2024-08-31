05feafac00630fa6a85c5785db49b1dd6706b8f0
Saketh Kotamraju <saketh.kotamraju@gmail.com>
2024-08-31 00:36:15 -0500

    Update core.py

--- modules/core.py
+++ modules/core.py
@@ -17,7 +17,7 @@ import tensorflow
 
 import modules.globals
 import modules.metadata
-import modules.ui as ui
+# import modules.ui as ui
 from modules.processors.frame.core import get_frame_processors_modules
 from modules.utilities import has_image_extension, is_image, is_video, detect_fps, create_video, extract_frames, get_temp_frame_paths, restore_audio, create_temp, move_temp, clean_temp, normalize_output_path
 
